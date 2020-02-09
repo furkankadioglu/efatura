2b68ec881d0fb1bbd2ddcea33c2f70d2be3d3b8c
Furkan KADIOGLU <iam@frkn.com.tr>
2020-02-10 02:45:43 +0300

    Ülke, para birimi, fatura tipi ve birimlerin sabit değişkenleri eklendi.

---
+++ src/Models/InvoiceType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace furkankadioglu\eFatura\Models;
+
+class InvoiceType {
+    const SATIS = "SATIS";
+    const IADE = "IADE";
+    const TEVKIFAT = "TEVKIFAT";
+    const ISTISNA = "ISTISNA";
+    const OZEL_MATRAH = "OZELMATRAH";
+    const IHRAC_KAYITLI = "IHRACKAYITLI";
+    const HAL_TIPI_FATURA_SATIS = "HKSSATIS";
+    const HAL_TIPI_FATURA_KOMISYONCU = "HKSKOMISYONCU";
+    
+}
---
+++ src/Models/UnitType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace furkankadioglu\eFatura\Models;
+
+final class UnitType {
+    const GUN = "DAY";
+    const AY = "MON";
+    const YIL = "ANN";
+    const SAAT = "HUR";
+    const DAKIKA = "D61";
+    const SANIYE = "D62";
+    const ADET = "C62";
+    const PAKET = "PA";
+    const KUTU = "BX";
+    const MG = "MGM";
+    const G = "GRM";
+    const KG = "KGM";
+    const LT = "LTR";
+    const TON = "TNE";
+    const NET_TON = "NT";
+    const GROSS_TON = "GT";
+    const MM = "MMT";
+    const CM = "CMT";
+    const M = "MTR";
+    const KM = "KTM";
+    const ML = "MLT";
+    const MM3 = "MMQ";
+    const CM2 = "CMK";
+    const CM3 = "CMQ";
+    const M2 = "MTK";
+    const M3 = "MTQ";
+    const KJ = "KJO";
+    const CL = "CLT";
+    const KARAT = "CT";
+    const KWH = "KWH";
+    const TON_BASINA_TASIMA_KAPASITESI = "CCT";
+    const BRUT_KALORI = "D30";
+    const BIN_LT = "D40";
+    const SAF_ALKOL_LT = "LPA";
+    const KGM2 = "B32";
+    const HUCRE_AT = "NCL";
+    const CIFT = "PR";
+    const BIN_M3 = "R9";
+    const SET = "SET";
+    const BIN_ADET = "T3";
+    
+}
